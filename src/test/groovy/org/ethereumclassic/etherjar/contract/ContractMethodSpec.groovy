c54e2b9db9be951681c93ebc3574cdf914691853
Dmitry Ulanov (aka dulanov) <du@iq2-development.de>
2016-11-16 12:38:05 +0100

    Validate a contract method signature

--- src/test/groovy/org/ethereumclassic/etherjar/contract/ContractMethodSpec.groovy
+++ src/test/groovy/org/ethereumclassic/etherjar/contract/ContractMethodSpec.groovy
@@ -9,6 +9,26 @@ import spock.lang.Specification
  */
 class ContractMethodSpec extends Specification {
 
+    def "check method signature validity"() {
+        expect:
+        ContractMethod.Builder.isSignatureValid(valid_sign as String)
+        where:
+        _ | valid_sign
+        _ | 'baz(uint32,bool)'
+        _ | 'bar(fixed128x128[2])'
+        _ | 'f123(uint256,uint32[],bytes10,bytes)'
+    }
+
+    def "check method signature invalidity"() {
+        expect:
+        !ContractMethod.Builder.isSignatureValid(invalid_sign as String)
+        where:
+        _ | invalid_sign
+        _ | 'baz(uint32, bool)'
+        _ | 'bar(fixed128x128[2]'
+        _ | '1f(uint256,uint32[],bytes10,bytes)'
+    }
+
     def "prepare method id"() {
         expect:
         def m = new ContractMethod.Builder().fromFullName(method).build()
