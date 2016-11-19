3864fdc4ea8b2740fb3933c74eabe95c7ecf1932
Constantine <kostiyantynk@gmail.com>
2016-11-20 00:09:09 +0100

    Tests for Contract and MethodId (#2)
    * Tests for Contract and MethodId
    
    * Updated tests for Contract and MethodId

--- src/test/groovy/org/ethereumclassic/etherjar/contract/ContractSpec.groovy
+++ src/test/groovy/org/ethereumclassic/etherjar/contract/ContractSpec.groovy
@@ -1,6 +1,56 @@
 package org.ethereumclassic.etherjar.contract
 
+import org.ethereumclassic.etherjar.model.MethodId
 import spock.lang.Specification
 
 class ContractSpec extends Specification {
+    def "check valid method search"() {
+        when:
+        Contract contract = new Contract(valid_methods as Collection<ContractMethod>)
+        then:
+        for (ContractMethod method: valid_methods)
+            contract.getMethod(method.getId()) == method
+        where:
+        _ | valid_methods
+        _ | [new ContractMethod(new MethodId([0xff, 0x11, 0x22, 0x33] as byte[])),
+             new ContractMethod(new MethodId([0xff, 0x00, 0xCC, 0x44] as byte[])),
+             new ContractMethod(new MethodId([0xff, 0xAA, 0x2B, 0x3C] as byte[]))]
+        _ | []
+    }
+
+    def "check invalid method search"() {
+        when:
+        Contract contract = new Contract([new ContractMethod(new MethodId([0xff, 0x11, 0x22, 0x33] as byte[])),
+                                          new ContractMethod(new MethodId([0xff, 0x00, 0xCC, 0x44] as byte[]))])
+        then:
+            contract.getMethod(invalid_methodId as MethodId) == null
+        where:
+        _ | invalid_methodId
+        _ | new MethodId([0xff, 0xff, 0xff, 0xff] as byte[])
+        _ | new MethodId([0x11, 0x11, 0x11, 0x11] as byte[])
+    }
+
+    def "check null method id"() {
+        when:
+        Contract contract = new Contract([new ContractMethod(new MethodId([0xff, 0x11, 0x22, 0x33] as byte[])),
+                                          new ContractMethod(new MethodId([0xff, 0x00, 0xCC, 0x44] as byte[]))])
+        contract.getMethod(null)
+        then:
+        thrown(IllegalArgumentException)
+
+    }
+
+    def "check get methods"() {
+        when:
+        Contract contract = new Contract(valid_methods as Collection<ContractMethod>)
+        then:
+        contract.getMethods().containsAll(valid_methods)
+        true
+        where:
+        _ | valid_methods
+        _ | [new ContractMethod(new MethodId([0xff, 0x11, 0x22, 0x33] as byte[])),
+             new ContractMethod(new MethodId([0xff, 0x00, 0xCC, 0x44] as byte[])),
+             new ContractMethod(new MethodId([0xff, 0xAA, 0x2B, 0x3C] as byte[]))]
+        _ | []
+    }
 }
--- src/test/groovy/org/ethereumclassic/etherjar/model/MethodIdSpec.groovy
+++ src/test/groovy/org/ethereumclassic/etherjar/model/MethodIdSpec.groovy
@@ -3,6 +3,68 @@ package org.ethereumclassic.etherjar.model
 import spock.lang.Specification
 
 class MethodIdSpec extends Specification {
+    def "check invalid bytes"() {
+        when:
+        new MethodId(invalid_bytes as byte[])
+        then:
+        thrown(IllegalArgumentException)
+        where:
+        _ | invalid_bytes
+        _ | [0x00]
+        _ | []
+        _ | [0x00, 0x01, 0x00, 0x00, 0xFF]
+    }
+
+    def "check from invalid bytes"() {
+        when:
+        MethodId.from(invalid_bytes as byte[])
+        then:
+        thrown(IllegalArgumentException)
+        where:
+        _ | invalid_bytes
+        _ | null
+        _ | [0x00]
+        _ | []
+        _ | [0x00, 0x01, 0x00, 0x00, 0xFF]
+    }
+
+    def "check from invalid string"() {
+        when:
+        MethodId.from(invalid_string as String)
+        then:
+        thrown(IllegalArgumentException)
+        where:
+        _ | invalid_string
+        _ | null
+        _ | "123"
+        _ | "-850932852093457982375"
+    }
+
+    def "check from valid bytes"() {
+        when:
+        MethodId id = MethodId.from(valid_bytes as byte[])
+        then:
+        id.getBytes() == valid_bytes
+        where:
+        _ | valid_bytes
+        _ | [(byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00]
+        _ | [(byte)0x01, (byte)0x00, (byte)0x00, (byte)0x0F]
+        _ | [(byte)0xFF, (byte)0xaa, (byte)0xcc, (byte)0x1c]
+        _ | [(byte)0x13, (byte)0x3b, (byte)0xf2, (byte)0x2F]
+    }
+
+    def "check from valid string"() {
+        when:
+        MethodId id = MethodId.from(valid_string as String)
+        then:
+        id.toString()equalsIgnoreCase(valid_string)
+        where:
+        _ | valid_string
+        _ | "0x11223344"
+        _ | "0x00000000"
+        _ | "0xABCDEFFF"
+        _ | "0x12345678"
+    }
 
     def "check method signature validity"() {
         expect:
