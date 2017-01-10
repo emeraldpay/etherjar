8d9593eca88376381b564ae4b51e8fc8d6f7e57d
dulanov <dulanov@gmail.com>
2017-01-10 23:48:59 +0100

    ByteType impl.

---
+++ src/main/java/org/ethereumclassic/etherjar/contract/type/BytesType.java
@@ -0,0 +1,75 @@
+package org.ethereumclassic.etherjar.contract.type;
+
+import org.ethereumclassic.etherjar.model.Hex32;
+import org.ethereumclassic.etherjar.model.HexData;
+
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ *  Dynamic sized byte sequence.
+ */
+public class BytesType implements DynamicType<byte[]> {
+
+    /**
+     * Try to parse a {@link BytesType} string representation (either canonical form or not).
+     *
+     * @param str a string
+     * @return a {@link BytesType} instance is packed as {@link Optional} value,
+     * or {@link Optional#empty()} instead
+     * @throws NullPointerException if a {@code str} is <code>null</code>
+     *
+     * @see #getCanonicalName()
+     */
+    public static Optional<BytesType> from(String str) {
+        Objects.requireNonNull(str);
+
+        return Objects.equals(str, "bytes") ?
+                Optional.of(new BytesType()) : Optional.empty();
+    }
+
+    @Override
+    public String getCanonicalName() {
+        return "bytes";
+    }
+
+    @Override
+    public HexData encode(byte... bytes) {
+        if (bytes.length == 0)
+            throw new IllegalArgumentException("Empty array of bytes to encode");
+
+        HexData len = Type.encodeLength(bytes.length);
+
+        return len.concat(new HexData(bytes),
+                new HexData(new byte[Hex32.SIZE_BYTES - (bytes.length % Hex32.SIZE_BYTES)]));
+    }
+
+    @Override
+    public byte[] decode(HexData data) {
+        int len = Type.decodeLength(data.extract(Hex32.SIZE_BYTES, Hex32::from)).intValueExact();
+
+        if (data.getSize() < len + Hex32.SIZE_BYTES)
+            throw new IllegalArgumentException("Insufficient data to decode bytes: " + data);
+
+        return data.extract(len, Hex32.SIZE_BYTES).getBytes();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClass());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (Objects.isNull(obj)) return false;
+
+        return Objects.equals(getClass(), obj.getClass());
+    }
+
+    @Override
+    public String toString() {
+        return getCanonicalName();
+    }
+}
