af8dd94133dc11eacfd5d69da4243e07d98e51cd
Igor Artamonov <igor@artamonov.ru>
2020-12-02 20:41:41 -0500

    solution: create eth_call request from ContractReadCall

--- etherjar-contract/src/main/java/io/emeraldpay/etherjar/contract/ContractReadCall.java
+++ etherjar-contract/src/main/java/io/emeraldpay/etherjar/contract/ContractReadCall.java
@@ -2,6 +2,9 @@ package io.emeraldpay.etherjar.contract;
 
 import io.emeraldpay.etherjar.domain.Address;
 import io.emeraldpay.etherjar.hex.HexData;
+import io.emeraldpay.etherjar.rpc.Commands;
+import io.emeraldpay.etherjar.rpc.RpcCall;
+import io.emeraldpay.etherjar.rpc.json.BlockTag;
 
 import java.util.function.Function;
 
@@ -20,5 +23,26 @@ public class ContractReadCall<T> extends ContractCall {
 
     public T processResult(HexData result) {
         return processor.apply(result);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Class<T> getTargetClass() {
+        return (Class<T>) Object.class;
+    }
+
+    public RpcCall<String, T> toRpcCall() {
+        return toRpcCall(BlockTag.LATEST);
+    }
+
+    public RpcCall<String, T> toRpcCall(BlockTag block) {
+        return Commands.eth()
+            .call(this.toJson(), block)
+            .converted(getTargetClass(), (hex) -> processResult(HexData.from(hex)));
+    }
+
+    public RpcCall<String, T> toRpcCall(Long height) {
+        return Commands.eth()
+            .call(this.toJson(), height)
+            .converted(getTargetClass(), (hex) -> processResult(HexData.from(hex)));
     }
 }
