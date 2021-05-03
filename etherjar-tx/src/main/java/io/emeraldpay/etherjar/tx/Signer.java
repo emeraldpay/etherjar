cb02fbe7ef60d5ac2567c082748e3b25ed4769ea
Igor Artamonov <igor@artamonov.ru>
2021-05-03 19:12:14 -0400

    solution: support for message signing (EIP-191)

--- etherjar-tx/src/main/java/io/emeraldpay/etherjar/tx/Signer.java
+++ etherjar-tx/src/main/java/io/emeraldpay/etherjar/tx/Signer.java
@@ -15,12 +15,17 @@
  */
 package io.emeraldpay.etherjar.tx;
 
+import io.emeraldpay.etherjar.domain.Address;
+import io.emeraldpay.etherjar.hex.Hex32;
+import io.emeraldpay.etherjar.hex.HexData;
+import io.emeraldpay.etherjar.hex.HexQuantity;
 import org.bouncycastle.asn1.x9.X9ECParameters;
 import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.ec.CustomNamedCurves;
 import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.crypto.signers.ECDSASigner;
 import org.bouncycastle.crypto.signers.HMacDSAKCalculator;
+import org.bouncycastle.jcajce.provider.digest.Keccak;
 import org.bouncycastle.math.ec.ECAlgorithms;
 import org.bouncycastle.math.ec.ECFieldElement;
 import org.bouncycastle.math.ec.ECPoint;
@@ -50,6 +55,113 @@ public class Signer {
 
     public static Signer newMainnet() {
         return new Signer(1);
+    }
+
+    /**
+     * Sign a message with Private Key as by EIP-191
+     *
+     * @param msg message to sign
+     * @param pk signer private key
+     * @return signature
+     * @see <a href="https://eips.ethereum.org/EIPS/eip-191">EIP-191</a>
+     */
+    public Signature signMessage(String msg, PrivateKey pk) {
+        return signMessage(msg.getBytes(), pk);
+    }
+
+    /**
+     * Sign a message with Private Key as by EIP-191
+     *
+     * @param msg message to sign
+     * @param pk signer private key
+     * @return signature
+     * @see <a href="https://eips.ethereum.org/EIPS/eip-191">EIP-191</a>
+     */
+    public Signature signMessage(byte[] msg, PrivateKey pk) {
+        byte[] hash = getMessageHash(msg);
+        return create(hash, pk, Signature.class);
+    }
+
+    /**
+     * Sign and encode a message with Private Key as by EIP-191
+     *
+     * @param msg message to sign
+     * @param pk signer private key
+     * @return signature
+     * @see <a href="https://eips.ethereum.org/EIPS/eip-191">EIP-191</a>
+     */
+    public HexData signMessageEncoded(String msg, PrivateKey pk) {
+        return signMessageEncoded(msg.getBytes(), pk);
+    }
+
+    /**
+     * Sign and encode a message with Private Key as by EIP-191
+     *
+     * @param msg message to sign
+     * @param pk signer private key
+     * @return signature
+     * @see <a href="https://eips.ethereum.org/EIPS/eip-191">EIP-191</a>
+     */
+    public HexData signMessageEncoded(byte[] msg, PrivateKey pk) {
+        Signature signature = signMessage(msg, pk);
+
+        return Hex32.extendFrom(toBytes(signature.getR()))
+            .concat(Hex32.extendFrom(toBytes(signature.getS())))
+            .concat(HexQuantity.from((long)signature.getV()).asData());
+    }
+
+    /**
+     * Verify message signed as EIP-191
+     *
+     * @param msg original message
+     * @param encodedSignature signature
+     * @param signer address of the signer
+     * @return true if signature is valid
+     */
+    public boolean verifyMessageSignature(String msg, HexData encodedSignature, Address signer) {
+        return verifyMessageSignature(msg.getBytes(), encodedSignature, signer);
+    }
+
+    /**
+     * Verify message signed as EIP-191
+     *
+     * @param msg original message
+     * @param encodedSignature signature
+     * @param signer address of the signer
+     * @return true if signature is valid
+     */
+    public boolean verifyMessageSignature(byte[] msg, HexData encodedSignature, Address signer) {
+        if (encodedSignature.getSize() < Hex32.SIZE_BYTES + Hex32.SIZE_BYTES + 1) {
+            throw new IllegalArgumentException("Signature is too short");
+        }
+        BigInteger r = new BigInteger(1, Hex32.from(encodedSignature.extract(Hex32.SIZE_BYTES)).getBytes());
+        BigInteger s = new BigInteger(1, Hex32.from(encodedSignature.extract(Hex32.SIZE_BYTES, Hex32.SIZE_BYTES)).getBytes());
+        BigInteger v = encodedSignature.extract(encodedSignature.getSize() - (Hex32.SIZE_BYTES + Hex32.SIZE_BYTES), Hex32.SIZE_BYTES + Hex32.SIZE_BYTES).asQuantity().getValue();
+        if (v.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0) {
+            throw new IllegalStateException("V is too large for int value: " + v);
+        }
+        byte[] hash = getMessageHash(msg);
+        Signature signatureDetails = new Signature(hash, v.intValue(), r, s);
+        return signatureDetails.recoverAddress().equals(signer);
+    }
+
+    protected byte[] getMessageHash(byte[] msg) {
+        Keccak.Digest256 digest = new Keccak.Digest256();
+        digest.update((byte)0x19);
+        digest.update("Ethereum Signed Message:\n".getBytes());
+        digest.update(Integer.toString(msg.length).getBytes());
+        digest.update(msg);
+        return digest.digest();
+    }
+
+    private byte[] toBytes(BigInteger value) {
+        byte[] b = value.toByteArray();
+        if (b[0] == 0x00) {
+            byte[] tail = new byte[b.length - 1];
+            System.arraycopy(b, 1, tail, 0, tail.length);
+            return tail;
+        }
+        return b;
     }
 
     public Signature sign(Transaction tx, PrivateKey pk) {
@@ -82,16 +194,14 @@ public class Signer {
         }
         byte[] publicKey = key.getPublicKey();
 
+        int y = getY(hash, r, s, publicKey);
         if (SignatureEIP155.class.equals(type)) {
-            int y = getY(hash, r, s, publicKey);
             return (T) new SignatureEIP155(chainId, hash, Eip155.toV(y, chainId), r, s);
         }
         if (SignatureEIP2930.class.equals(type)) {
-            int y = getY(hash, r, s, publicKey);
             return (T) new SignatureEIP2930(hash, y, chainId, r, s);
         }
-
-        return (T) new Signature(hash, 27, r, s);
+        return (T) new Signature(hash, 27 + y, r, s);
     }
 
     public int getY(byte[] hash, BigInteger r, BigInteger s, byte[] publicKey) {
--- etherjar-tx/src/test/groovy/io/emeraldpay/etherjar/tx/SignerSpec.groovy
+++ etherjar-tx/src/test/groovy/io/emeraldpay/etherjar/tx/SignerSpec.groovy
@@ -273,4 +273,87 @@ class SignerSpec extends Specification {
         then:
         Hex.encodeHexString(pubkey) == "653952d0020981cf0332c34fe3c931a8fc4c85e4380eb9c11bc16dcdcaae6ef961b4461519c04dd83b4d2937b2585f6734ef074b3859d58b0093183858ab0ca1"
     }
+
+    def "Sign message"() {
+        setup:
+        PrivateKey pk = PrivateKey.create("0x4646464646464646464646464646464646464646464646464646464646464646")
+        def message = "test-test-test"
+
+        when:
+        def act = signer.signMessageEncoded(message, pk)
+
+        then:
+        act.toHex() == "0xc26a3a1922d97e573db507e82cbace7b57e54106cc96d598d29ac16aabe48153313302cb629b7307baae0ae5e74f68e58564615ccfde0d03603381e1a233e0ed1c"
+    }
+
+    def "Sign message 2"() {
+        setup:
+        PrivateKey pk = PrivateKey.create("0x4646464646464646464646464646464646464646464646464646464646464646")
+        def message = "test-test-test 2"
+
+        when:
+        def act = signer.signMessageEncoded(message, pk)
+
+        then:
+        act.toHex() == "0x86f13303ffc5c05b3bf500f7f6f8bce9074721ea792e41c9f3624318ee08eebc6c1e4c3f091b2c9611361d462af3103c64a6873918c1aacaf2171bd36615f9f61c"
+    }
+
+    def "Sign message 3"() {
+        setup:
+        PrivateKey pk = PrivateKey.create("0x4646464646464646464646464646464646464646464646464646464646464646")
+        def message = "test-test-test 3"
+
+        when:
+        def act = signer.signMessageEncoded(message, pk)
+
+        then:
+        act.toHex() == "0xb16541fb0a35a5415c9ddc59afd410b45af88c97e7ca7b172306e9513951279a64d8fc0e4efe055417e604244d53f538422f0b7c686c10133ebad1c91df2980d1b"
+    }
+
+    def "Sign message 4"() {
+        setup:
+        PrivateKey pk = PrivateKey.create("0xb16541fb0a35a5415c9ddc59afd410b45af88c97e7ca7b172306e9513f538422")
+        def message = "test b16541fb0a35a5415c9ddc59afd410b45af88c97e7ca7b172306e9513951279a64d8fc0e4efe055417e604244d53f538422f0b7c686c10133ebad1c91df2980d1b"
+
+        when:
+        def act = signer.signMessageEncoded(message, pk)
+
+        then:
+        act.toHex() == "0xde8d65f0d3de2fbdac8f9348b7e215bcaa7780f772ed28e7d6cdae458938b86b51411b1d50af102484b3bbd4cc1b8ace1ecbcd0747fbbf303d10beb579d67e4b1c"
+    }
+
+    def "Verify message signature 1"() {
+        setup:
+        def message = "test-test-test"
+        def address = Address.from("0x9d8A62f656a8d1615C1294fd71e9CFb3E4855A4F")
+        def addressWrong = Address.from("0x1c5E6f6F6C7866EF146B0c0220D857D12a9058F0")
+        def signature = HexData.from("0xc26a3a1922d97e573db507e82cbace7b57e54106cc96d598d29ac16aabe48153313302cb629b7307baae0ae5e74f68e58564615ccfde0d03603381e1a233e0ed1c")
+        when:
+        def act = signer.verifyMessageSignature(message, signature, address)
+        then:
+        act
+
+        when:
+        act = signer.verifyMessageSignature(message, signature, addressWrong)
+        then:
+        !act
+    }
+
+    def "Verify message signature 2"() {
+        setup:
+        def message = "test-test-test 2"
+        def address = Address.from("0x9d8A62f656a8d1615C1294fd71e9CFb3E4855A4F")
+        def addressWrong = Address.from("0x1c5E6f6F6C7866EF146B0c0220D857D12a9058F0")
+        def signature = HexData.from("0x86f13303ffc5c05b3bf500f7f6f8bce9074721ea792e41c9f3624318ee08eebc6c1e4c3f091b2c9611361d462af3103c64a6873918c1aacaf2171bd36615f9f61c")
+        when:
+        def act = signer.verifyMessageSignature(message, signature, address)
+        then:
+        act
+
+        when:
+        act = signer.verifyMessageSignature(message, signature, addressWrong)
+        then:
+        !act
+    }
+
 }
