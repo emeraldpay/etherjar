9928291a384f52010741e29aed8c621f3ad2d037
Igor Artamonov <igor@artamonov.ru>
2021-07-07 13:52:26 -0400

    solution: shortcut to convert a ContractCall to a Transaction

--- etherjar-contract/src/main/java/io/emeraldpay/etherjar/contract/ContractCall.java
+++ etherjar-contract/src/main/java/io/emeraldpay/etherjar/contract/ContractCall.java
@@ -2,6 +2,7 @@ package io.emeraldpay.etherjar.contract;
 
 import io.emeraldpay.etherjar.domain.Address;
 import io.emeraldpay.etherjar.rpc.json.TransactionCallJson;
+import io.emeraldpay.etherjar.tx.Transaction;
 
 import java.util.Objects;
 
@@ -23,11 +24,29 @@ public class ContractCall {
         return data;
     }
 
+    /**
+     * Convert to a JSON-RPC request, suitable for a <em>read call</em>.
+     *
+     * @return JSON RPC request
+     */
     public TransactionCallJson toJson() {
         TransactionCallJson json = new TransactionCallJson();
         json.setTo(contract);
         json.setData(data.toData());
         return json;
+    }
+
+    /**
+     * Convert to an <em>unsigned</em> transaction, without Gas Amount, Gas Price, etc. I.e., only
+     * contract data and address are set, and all other fields are supposed to be set by sender.
+     *
+     * @return pre-filled transaction
+     */
+    public Transaction toTransaction() {
+        Transaction tx = new Transaction();
+        tx.setTo(contract);
+        tx.setData(data.toData());
+        return tx;
     }
 
     @Override
